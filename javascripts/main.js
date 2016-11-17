c5d90caa9c0415a811ef635b47dfed30ac40854f
juliakimchung <julhkm@gmail.com>
2016-11-17 17:29:47 -0600

    my first firebase

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,26 +9,64 @@ let $ = require('jquery'),
 // Using the REST API
 function loadSongsToDOM() {
   console.log("Need to load some songs, Buddy");
+  db.getSongs()
+  .then(function(songData){
+    console.log("got Song Data", songData);
+    var idArray = Object.keys(songData);
+    idArray.forEach(function(key){
+      songData[key].id = key;
+    });
+    console.log("song object with ID", songData);
+    templates.makeSongList(songData);
+  });
 }
 loadSongsToDOM(); //<--Move to auth section after adding login btn
 
 // Send newSong data to db then reload DOM with updated song data
 $(document).on("click", ".save_new_btn", function() {
+  let songObj = buildSongObj();
+  db.addSong(songObj)
+  .then(function(songID) {
+    loadSongsToDOM();
+  });
 
 });
 
 // go get the song from database and then populate the form for editing.
 $(document).on("click", ".edit-btn", function () {
+  console.log("click on edit button");
+  let songID = $(this).data("edit-id");
+  db.getSong(songID)
+  .then(function(song){
+    return templates.songForm(song, songID);
+  })
+  .then(function(finishedForm) {
+    $(".uiContainer--wrapper").html(finishedForm);
+  });
 
 });
 
 //Save edited song to FB then reload DOM with updated song data
 $(document).on("click", ".save_edit_btn", function() {
+  console.log("click save edit button");
+  let songObj = buildSongObj(),
+  songID = $(this).attr("id");
+  db.editSong(songObj, songID)
+  .then(function(data){
+    loadSongsToDOM(data);
+  });
+
 
 });
 
 // Remove song then reload the DOM w/out new song
 $(document).on("click", ".delete-btn", function () {
+  console.log("click delete-btn");
+  let songId =  $(this).data("delete-id");
+  db.deleteSong(songId)
+  .then(function() {
+    loadSongsToDOM();
+  });
 
 });
 
